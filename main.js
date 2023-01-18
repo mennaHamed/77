57ac3cc148d31d323200115536bfdb474f1712b5
menna hamed <112440501+mennaHamed@users.noreply.github.com>
2023-01-18 20:59:44 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+
+function toggleMenu(){
+    const menuToggle=document.querySelector('.menuToggle');
+    const navigation=document.querySelector('.navigation')
+    menuToggle.classList.toggle('active');
+    navigation.classList.toggle('active');
+}
